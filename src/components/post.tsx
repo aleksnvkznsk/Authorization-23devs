b0279eab5d8c264eabff8b3febad9ed3d8ec0031
aleksnvkznsk <aleksandrov.developer@gmail.com>
2023-08-07 22:09:49 +0700

    add Material UI

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,65 +1,25 @@
-import { data } from '../components/json';
-import { useState } from 'react';
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 import style from './../style/post.module.css'
 
-const Post = () => {
-    const [state, setState] = useState('');
+export default function PaginationControlled() {
+    const [page, setPage] = React.useState(1);
+    const handleChange = (_event: React.ChangeEvent<unknown>, value: number) => {
+        setPage(value);
+    };
 
     return (
         <div className={style.post}>
-            <div className={style.post__buttons}>
-                <button className={style.post__buttons__button} onClick={() => { setState('b1') }}>1</button>
-                <button className={style.post__buttons__button} onClick={() => { setState('b2') }}>2</button>
-                <button className={style.post__buttons__button} onClick={() => { setState('b3') }}>3</button>
-                <button className={style.post__buttons__button} onClick={() => { setState('b4') }}>4</button>
-                <button className={style.post__buttons__button} onClick={() => { setState('b5') }}>5</button>
-                <button className={style.post__buttons__button} onClick={() => { setState('b6') }}>6</button>
-                <button className={style.post__buttons__button} onClick={() => { setState('b7') }}>7</button>
-                <button className={style.post__buttons__button} onClick={() => { setState('b8') }}>8</button>
-                <button className={style.post__buttons__button} onClick={() => { setState('b9') }}>9</button>
-                <button className={style.post__buttons__button} onClick={() => { setState('b10') }}>10</button>
-            </div>
-
-            {state === '' &&
-                <div className={style.list}>
-                    {data.map(() => {
-                        return (
-                            <div className={style.post__container}>
-                                <p className={style.post__title}>{data[0].title}</p>
-                                <p className={style.post__body}>{data[0].body}</p>
-                            </div>
-                        )
-                    })}
-                </div>
-            }
-
-            {state === 'b1' &&
-                <div className={style.list}>
-                    {data.map(() => {
-                        return (
-                            <div className={style.post__container}>
-                                <p className={style.post__title}>{data[0].title}</p>
-                                <p className={style.post__body}>{data[0].body}</p>
-                            </div>
-                        )
-                    })}
-                </div>
-            }
-
-            {state === 'b2' &&
-                <div className={style.list}>
-                    {data.map(() => {
-                        return (
-                            <div className={style.post__container}>
-                                <p className={style.post__title}>{data[1].title}</p>
-                                <p className={style.post__body}>{data[1].body}</p>
-                            </div>
-                        )
-                    })}
-                </div>
-            }
+            <Stack className={style.stack} spacing={2}>
+                <Typography>
+                    <div className={style.list}>
+                        Содержимое
+                    </div>
+                </Typography>
+                <Pagination count={10} page={page} color="secondary" onChange={handleChange} />
+            </Stack>
         </div>
     );
 }
-
-export default Post

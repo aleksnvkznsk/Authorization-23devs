500a9ce741c7b1fcd6ddb394ea24e9b4d4dec609
aleksnvkznsk <aleksandrov.developer@gmail.com>
2023-08-09 18:21:27 +0700

    create pagination

--- src/components/posts/Post.tsx
+++ src/components/posts/Post.tsx
@@ -1,10 +1,17 @@
+import PaginationControlled from '../pagination/Pagination';
 import style from './post.module.css';
+
+
 
 const Post = () => {
 
     return (
         <div className={style.post}>
-            
+            <div className={style.post__container}>
+                <p className={style.post__title}>title</p>
+                <p className={style.post__body}>body</p>
+            </div>
+            <PaginationControlled />
         </div>
     );
 }
